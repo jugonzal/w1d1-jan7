f4a79e1089de3a6f3d0ddf905c9f423258956067
Juan Gonzalez <jugonzal@gmail.com>
2019-01-07 16:37:18 -0500

    Added support for Special Symbols and length

--- pwdzer.js
+++ pwdzer.js
@@ -8,20 +8,35 @@ console.log('Password to analyze: ', pwd)
 
 // - Should include capital letters
 
-var allCapitals = "QWERTYUIOPASDFGHJKLZXCVBNM"
-var areThereCapitals = false
-for (var p = 0; p < pwd.length; p++) {
-  if (allCapitals.includes(pwd[p])) { // is a capital letter
-    areThereCapitals = true
+// var allCapitals = "QWERTYUIOPASDFGHJKLZXCVBNM"
+// var areThereCapitals = false
+// for (var p = 0; p < pwd.length; p++) {
+//   if (allCapitals.includes(pwd[p])) { // is a capital letter
+//     areThereCapitals = true
+//   }
+// }
+
+function areThereAny(fullset, word) {
+  var areThere = false
+  for (var p = 0; p < word.length; p++) {
+    if (fullset.includes(word[p])) { // is a capital letter
+      return true
+    }
   }
+  return false
 }
 
-console.log('Does it have Capitals? ', areThereCapitals)
+
+var allCapitals = "QWERTYUIOPASDFGHJKLZXCVBNM"
+console.log('Does it have Capitals? ', areThereAny(allCapitals,pwd))
 
 // - Should be really long...  13 characters
+
+console.log('Is it really long? ',pwd.length>13?true:false)
 // - should not have your name
 // - no dictionary words
 // - replace numbers for certain letters...   A -> 4   O -> 0
 // - should include some special characters...  !!!
 
+console.log('Does it have Special symbols? ',areThereAny("!@#%$^&*()[]{}",pwd))
 
