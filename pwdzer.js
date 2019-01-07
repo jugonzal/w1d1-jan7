2855e76aa83f65276c2143accc790c2c4ee477ee
Juan Gonzalez <jugonzal@gmail.com>
2019-01-07 16:05:12 -0500

    just started the main code file

---
+++ pwdzer.js
@@ -0,0 +1,10 @@
+// I am coding here my password analyzer
+
+// - Should include capital letters
+// - Should be really long...  13 characters
+// - should not have your name
+// - no dictionary words
+// - replace numbers for certain letters...   A -> 4   O -> 0
+// - should include some special characters...  !!!
+
+
