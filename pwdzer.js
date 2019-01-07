7b3085d5f59d4992413e12a32944127d5c041b51
Juan Gonzalez <jugonzal@gmail.com>
2019-01-07 16:28:34 -0500

    Capitals checked

--- pwdzer.js
+++ pwdzer.js
@@ -2,11 +2,22 @@
 
 // first step is to collect the password from command line
 
-var pwd = process.argv
+var pwd = process.argv[2]
 
 console.log('Password to analyze: ', pwd)
 
 // - Should include capital letters
+
+var allCapitals = "QWERTYUIOPASDFGHJKLZXCVBNM"
+var areThereCapitals = false
+for (var p = 0; p < pwd.length; p++) {
+  if (allCapitals.includes(pwd[p])) { // is a capital letter
+    areThereCapitals = true
+  }
+}
+
+console.log('Does it have Capitals? ', areThereCapitals)
+
 // - Should be really long...  13 characters
 // - should not have your name
 // - no dictionary words
